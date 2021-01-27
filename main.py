c1733f3188288d1addaee8edbac9f0a31e25359e
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 17:12:39 -0500

    converts hex and dec to ascii

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ Author: mp
 
 # take first value to be converted
 print("Convert what type of value?\n")
-print("Choose b for binary or d for decimal\n")
+print("Choose b for binary, d for decimal, or h for hex\n")
 convertfrom = str(input())
 print("Ok, please enter the value: ")
-start_value = int(input())
+if convertfrom == "h":
+	start_value = input()
+else:
+	start_value = int(input())
 print(f"Thank you, we will start with {start_value}.\n")
 
 # choose what to convert the value into
 print("And what would you like to convert this to?\n")
-print("Choose d for decimal or h for hex\n")
+print("Choose d for decimal, h for hex, or a for ascii\n")
 convert2 = str(input())
 
 dnum = 0
@@ -23,8 +26,7 @@ i = 1
 # runs the conversions
 if convertfrom == "d" and convert2 == "h":
 	print("\n" + hex(start_value))
-elif convertfrom == "h":
-	pass
+
 elif convertfrom == "b" and convert2 == "d":
 	while start_value != 0:
 		rem = start_value % 10
@@ -33,9 +35,11 @@ elif convertfrom == "b" and convert2 == "d":
 		start_value = int(start_value/10)
 
 	print("\nEquivalent Decimal Value = ", dnum)
-	
-elif convertfrom == "a":
-	pass
+
+elif convertfrom == "d" and convert2 == "a":
+	print(chr(start_value))
+elif convertfrom == "h" and convert2 == "a":
+	print(bytearray.fromhex(start_value).decode())
 else:
 	Print("The input was not correct, please try again.")
 	quit()
