fdbee47920d81066ce63c0d7140b97c56f7f840a
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 15:51:14 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+A simple converter tool.
+Converts decimal, hex, binary, and ascii.
+
+Author: mp
+'''
+
+# take first value to be converted
+print("Convert what type of code?\n")
+print("Choose d for decimal, h for hex, b for binary, or a for ascii\n")
+convertfrom = input()
+print("")
+start_value = input()
+print("Thank you, we will start with {start}.\n")
+print("And what would you like to convert this to?\n")
+print("Choose d for decimal")
+
