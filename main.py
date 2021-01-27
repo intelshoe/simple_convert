f07f2c73c266d8f8d37a26caf532faa9842be8d1
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 18:14:25 -0500

    simple wording correction

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Author: mp
 
 # take first value to be converted
 print("Convert what type of value?\n")
-print("Choose b for binary, d for decimal, h for hex, or a for ascii\n")
+print("Type b for binary, d for decimal, h for hex, or a for ascii\n")
 convertfrom = str(input())
 print("Ok, please enter the value: ")
 if convertfrom == "h" or convertfrom == "a":
@@ -18,7 +18,7 @@ print(f"Thank you, we will start with {start_value}.\n")
 
 # choose what to convert the value into
 print("And what would you like to convert this to?\n")
-print("Choose b for binary, d for decimal, h for hex, or a for ascii\n")
+print("Type b for binary, d for decimal, h for hex, or a for ascii\n")
 convert2 = str(input())
 
 dnum = 0
@@ -38,17 +38,22 @@ elif convertfrom == "b" and convert2 == "d":
 
 elif convertfrom == "d" and convert2 == "a":
 	print(chr(start_value))
+
 elif convertfrom == "h" and convert2 == "a":
 	print(bytearray.fromhex(start_value).decode())
+
 elif convertfrom == "h" and convert2 == "d":
 	dnum = int(start_value, 16)
 	print(f"\n {dnum}")
+
 elif convertfrom == "d" and convert2 == "b":
 	dnum = bin(start_value)
 	print(f"\n {dnum}")
+
 elif convertfrom == "a" and convert2 == "d":
 	dnum=ord(start_value)
 	print(f"\n {dnum}")
+	
 else:
 	print("The input was not correct, please try again.")
 	quit()
