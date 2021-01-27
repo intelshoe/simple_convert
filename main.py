e3c05563cd74ec674a9748ce693212e7ec9d6d29
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 16:09:52 -0500

    testing binary to decimal

--- main.py
+++ main.py
@@ -6,25 +6,39 @@ Author: mp
 '''
 
 # take first value to be converted
+
+
+
 print("Convert what type of value?\n")
 print("Choose d for decimal, h for hex, b for binary, or a for ascii\n")
-convertfrom = input()
+convertfrom = str(input())
 print("Ok, please enter the value: ")
-start_value = input()
+start_value = int(input())
 print(f"Thank you, we will start with {start_value}.\n")
 
 # choose what to convert the value into
 print("And what would you like to convert this to?\n")
 print("Choose d for decimal, h for hex, b for binary, or a for ascii\n")
+convert2 = str(input())
 
+dnum = 0
+i = 1
+# runs the conversions
 if convertfrom == "d":
-
+	pass
 elif convertfrom == "h":
+	pass
+elif convertfrom == "b" and convert2 == "d":
+	while start_value!=0:
+    	rem = start_value%10
+    	dnum = dnum + (rem*i)
+    	i = i*2
+    	start_value = int(start_value/10)
 
-elif convertfrom == "b":
-
+	print("\nEquivalent Decimal Value = ", dnum)
+	
 elif convertfrom == "a":
-
+	pass
 else
 	Print("The input was not correct, please try again.")
 	quit()
