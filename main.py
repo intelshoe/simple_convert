c83e9f3ea08bfbaef8c7a51370b1239b9451f0d7
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 15:58:07 -0500

    setup input values

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ Author: mp
 '''
 
 # take first value to be converted
-print("Convert what type of code?\n")
+print("Convert what type of value?\n")
 print("Choose d for decimal, h for hex, b for binary, or a for ascii\n")
 convertfrom = input()
-print("")
+print("Ok, please enter the value: ")
 start_value = input()
-print("Thank you, we will start with {start}.\n")
-print("And what would you like to convert this to?\n")
-print("Choose d for decimal")
+print(f"Thank you, we will start with {start_value}.\n")
 
+# choose what to convert the value into
+print("And what would you like to convert this to?\n")
+print("Choose d for decimal, h for hex, b for binary, or a for ascii\n")
+
+if convertfrom == "d":
+
+elif convertfrom == "h":
+
+elif convertfrom == "b":
+
+elif convertfrom == "a":
+
+else
+	Print("The input was not correct, please try again.")
+	quit()
