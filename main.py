ba50f7d15e70d55e510a75f268fced859b3a0d8f
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 16:43:18 -0500

    add decimal to hex conversion

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ Author: mp
 '''
 
 # take first value to be converted
-
-
-
 print("Convert what type of value?\n")
-print("Choose b for binary\n")
+print("Choose b for binary or d for decimal\n")
 convertfrom = str(input())
 print("Ok, please enter the value: ")
 start_value = int(input())
@@ -18,14 +15,14 @@ print(f"Thank you, we will start with {start_value}.\n")
 
 # choose what to convert the value into
 print("And what would you like to convert this to?\n")
-print("Choose d for decimal\n")
+print("Choose d for decimal or h for hex\n")
 convert2 = str(input())
 
 dnum = 0
 i = 1
 # runs the conversions
-if convertfrom == "d":
-	pass
+if convertfrom == "d" and convert2 == "h":
+	print("\n" + hex(start_value))
 elif convertfrom == "h":
 	pass
 elif convertfrom == "b" and convert2 == "d":
