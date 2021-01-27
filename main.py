d41e321f70eb45e34cdbab6d9895f51c13ffd7ca
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-27 17:36:43 -0500

    added decimal to binary conversion

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ print(f"Thank you, we will start with {start_value}.\n")
 
 # choose what to convert the value into
 print("And what would you like to convert this to?\n")
-print("Choose d for decimal, h for hex, or a for ascii\n")
+print("Choose b for binary, d for decimal, h for hex, or a for ascii\n")
 convert2 = str(input())
 
 dnum = 0
@@ -43,7 +43,9 @@ elif convertfrom == "h" and convert2 == "a":
 elif convertfrom == "h" and convert2 == "d":
 	dnum = int(start_value, 16)
 	print(f"\n {dnum}")
-
+elif convertfrom == "d" and convert2 == "b":
+	dnum = bin(start_value)
+	print(f"\n {dnum}")
 elif convertfrom == "a" and convert2 == "d":
 	dnum=ord(start_value)
 	print(f"\n {dnum}")
